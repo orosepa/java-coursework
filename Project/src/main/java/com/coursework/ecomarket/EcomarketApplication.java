11f2c68864c05a5e29e27896c1c4d748770f224e
orosepa <l.olga.podgornaya@gmail.com>
2021-04-27 08:10:02 +0300

    Spring framework support added

---
+++ Project/src/main/java/com/coursework/ecomarket/EcomarketApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.ecomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomarketApplication.class, args);
+    }
+
+}
