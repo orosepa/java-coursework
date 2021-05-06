2a972a4b3868347abe041c66847eff5f5ef02a0a
orosepa <l.olga.podgornaya@gmail.com>
2021-05-06 10:03:09 +0300

    Set up config

---
+++ Project/src/main/java/com/coursework/ecomarket/configuration/UserPrincipalDetailService.java
@@ -0,0 +1,26 @@
+package com.coursework.ecomarket.configuration;
+
+import com.coursework.ecomarket.models.User;
+import com.coursework.ecomarket.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
+
